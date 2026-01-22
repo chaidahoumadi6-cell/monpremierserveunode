1cb0fe75591ca254a402ff317caca124dab872ea
chaidahoumadi6-cel <chaidahoumadi6@gmail.com>
2026-01-22 16:34:09 +0300

    J'ai relié les serveur et créer l'appl ication expressjs pour gérer les routes

---
+++ app.js
@@ -0,0 +1,16 @@
+// Ici nous créons notre application ExpressJs
+
+//J'importe le framework Expressjs.
+const express = require('express');
+
+//J'initialise une application express
+const app = express();
+
+app.use((req,res) =>{
+    res.end("Bonjour je suis le serveur!");
+});
+
+/*J'exporte l'application express sous forme de module
+Ainsi l'application app peut être importée dans d'autres fichiers Js.
+*/
+module.exports =app;
