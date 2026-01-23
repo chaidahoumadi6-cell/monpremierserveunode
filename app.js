11d36d98278e0dd98ec9fb7cba50b74799e498be
chaidahoumadi6-cel <chaidahoumadi6@gmail.com>
2026-01-23 09:48:33 +0300

    j'ai relie les serveur

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-// Ici nous créons notre application ExpressJs
+// Icin nous créos notre application ExpressJs
 
-//J'importe le framework Expressjs.
+// j'importe le framewordk Expressjs.
 const express = require('express');
 
-//J'initialise une application express
+// J'initialise une application ExpressJs
 const app = express();
 
-app.use((req,res) =>{
-    res.end("Bonjour je suis le serveur!");
+app.use((req, res) =>{
+    res.end("Bonjour, je suis le serveur!")
 });
 
-/*J'exporte l'application express sous forme de module
-Ainsi l'application app peut être importée dans d'autres fichiers Js.
+/*
+j'exporte l'application express sous forme de module
+Ainsi, l'application app peut être importée dans d'autres fichiers JS.
 */
-module.exports =app;
+module.exports = app;//fin du fichier. Donc ne pas coder en dessous de celui-ci
--- serveur.js
+++ serveur.js
@@ -1,14 +1,32 @@
-// Ici je code mon serveur
+// Ici, je code mon serveur0
 
-/*J'importe le package (module) http. Le module HTTP est stocké dans la variable HTTP
-L'instruction require() permet d'importer le module.children.
-*/
+/**
+ * J'importe le package (module) http. Le module HTTP est stocké dans la variable http.
+ * L'instruction require() permet d'importer le module.
+ */ 
 const http = require('http');
 
-//Maintenat,je créer mon serveur en utilisant le package HTTP
-const serveur = http.createServer((req,res) => {
-    res.end("Bonjour,je suis le serveur");
-});
+// j'importe l'application express app.js
+const app = require('./app');
 
+// Je configure le numéro du port utiliser dans le serveur
+app.set('port', 3000);
+
+//Je crée mon serveur en utilisant le package HTTP
+//req = requet, res = réponse
+
+const server = http.createServer(app);
+/*
+const server = http.createServer((req, res) => {
+    res.end("Bonjour, je suis le serveur !");
+});
+*/
+
+// Je récupère la date
+const date = new Date();
 // Je précise l'adresse et le port du serveur
-serveur.listen(3000);
+server.listen(3000, () => {
+    console.log("Le serveur tourne sur le port 3000.")
+    console.log(date.toLocaleDateString());
+    console.log( date.toLocaleTimeString());
+}); 
