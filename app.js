1bef3034198edff0d7af9cc86d87d63ff32c8b63
chaidahoumadi6-cel <chaidahoumadi6@gmail.com>
2026-01-26 10:58:45 +0300

    j'ai integrer les middleware dans la'app.js

--- app.js
+++ app.js
@@ -6,8 +6,25 @@ const express = require('express');
 // J'initialise une application ExpressJs
 const app = express();
 
-app.use((req, res) =>{
-    res.end("Bonjour, je suis le serveur!")
+// Middleware exécuté pour chaque requête reçue par le serveur
+app.use((req,res, next) => {
+
+    console.log("Je suis dans le serveur");
+
+    // Passe au middleware ou à la route suivante
+    next();
+});
+
+app.use((req,res) => {
+
+    // Envoie une réponse JSON au client avec un message
+    res.json({message:"Vous êtes bien sur le serveur"});
+
+});
+
+app.use((req, res) => {
+    res.console("Bonjour, je suis le serveur!");
+
 });
 
 /*
